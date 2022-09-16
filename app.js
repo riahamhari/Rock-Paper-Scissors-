8af0b55b85562c6f89291dd58d76cf4ac9160306
riahamhari <109351021+riahamhari@users.noreply.github.com>
2022-09-16 15:56:42 +0100

    refactoring and bug fixes

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const rock = document.querySelector('#rock')
-const paper = document.querySelector('#paper')
-const scissors = document.querySelector('#scissors')
-const playerDisplay = document.querySelector('#playerDisplay')
-const computerDisplay = document.querySelector('#computerDisplay')
+
+// GAME
 
 let playerScore = 0;
 let computerScore = 0;
 let winningScore = 5;
-// let computerChoice = ''
+let roundWinner = '';
+
+
+const codes = {
+    rock: '&#9994',
+    paper: '&#9995',
+    scissors: '&#9996'
+}
 
 
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3 + 1);
-    if (computerChoice === 1) {
-        return 'rock';
-    }
-    if (computerChoice === 2) {
-        return 'paper';
-    }
-    if (computerChoice === 3) {
-        return 'scissors';
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    switch (randomNumber) {
+        case 1:
+            return 'ROCK';
+        case 2:
+            return 'PAPER';
+        case 3:
+            return 'SCISSORS';
     }
 
 
@@ -27,143 +30,146 @@ function getComputerChoice() {
 
 
 
-// function playRound(playerSelection, computerSelection) {
 
-//   }
-
-
-rock.addEventListener('click', function () {
-    // let playerChoice = 'rock'
-    getComputerChoice()
-
-    if (getComputerChoice() === 'paper') {
-        computerScore += 1;
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        roundWinner = 'tie'
     }
-    if (getComputerChoice() === 'scissors') {
-        playerScore += 1;
+    if (
+        (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+    ) {
+        playerScore++
+        roundWinner = 'player'
     }
-    if (playerScore === winningScore) {
-        rock.style.opacity = "0.5";
-        paper.style.opacity = "0.5";
-        scissors.style.opacity = "0.5";
-
-        rock.disabled = "true";
-        paper.disabled = "true";
-        scissors.disabled = "true";
-        rock.style.cursor = "not-allowed";
-        paper.style.cursor = "not-allowed";
-        scissors.style.cursor = "not-allowed";
-        playerDisplay.style.color = "green";
-        computerDisplay.style.color = "red";
+    if (
+        (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
+        (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
+        (computerSelection === 'PAPER' && playerSelection === 'ROCK')
+    ) {
+        computerScore++
+        roundWinner = 'computer'
     }
-    if (computerScore === winningScore) {
-        rock.style.opacity = "0.5";
-        paper.style.opacity = "0.5";
-        scissors.style.opacity = "0.5";
+    updateScoreMessage(roundWinner, playerSelection, computerSelection)
+}
 
-        rock.disabled = "true";
-        paper.disabled = "true";
-        scissors.disabled = "true";
-        rock.style.cursor = "not-allowed";
-        paper.style.cursor = "not-allowed";
-        scissors.style.cursor = "not-allowed";
-        playerDisplay.style.color = "red";
-        computerDisplay.style.color = "green";
+
+
+
+
+function isGameOver() {
+    return playerScore === 5 || computerScore === 5
+}
+
+
+//   UI
+const scoreInfo = document.querySelector('#scoreInfo')
+const scoreMessage = document.querySelector('#scoreMessage')
+const playerSign = document.querySelector('#playerSign')
+const computerSign = document.querySelector('#computerSign')
+const playerScoreDisplay = document.querySelector('#playerScore')
+const computerScoreDisplay = document.querySelector('#computerScore')
+const rockBtn = document.querySelector('#rock')
+const paperBtn = document.querySelector('#paper')
+const scissorsBtn = document.querySelector('#scissors')
+
+rockBtn.addEventListener('click', () => btnClick('ROCK'))
+paperBtn.addEventListener('click', () => btnClick('PAPER'))
+scissorsBtn.addEventListener('click', () => btnClick('SCISSORS'))
+
+
+
+function btnClick(playerSelection) {
+    if (isGameOver()) {
+        rockBtn.style.opacity = "0.5";
+        paperBtn.style.opacity = "0.5";
+        scissorsBtn.style.opacity = "0.5";
+
+        rockBtn.disabled = "true";
+        paperBtn.disabled = "true";
+        scissorsBtn.disabled = "true";
+        rockBtn.style.cursor = "not-allowed";
+        paperBtn.style.cursor = "not-allowed";
+        scissorsBtn.style.cursor = "not-allowed";
+        return
     }
+    const computerSelection = getComputerChoice()
+    playRound(playerSelection, computerSelection)
+    updateChoices(playerSelection, computerSelection)
+    updateScore()
 
-    playerDisplay.innerText = playerScore;
-    computerDisplay.innerText = computerScore;
+    if (isGameOver()) {
+        rockBtn.disabled = "true";
+        paperBtn.disabled = "true";
+        scissorsBtn.disabled = "true";
+        rockBtn.style.cursor = "not-allowed";
+        paperBtn.style.cursor = "not-allowed";
+        scissorsBtn.style.cursor = "not-allowed";
+        return
 
-
-})
-
-paper.addEventListener('click', function () {
-
-    getComputerChoice()
-
-    if (getComputerChoice() === 'scissors') {
-        computerScore += 1;
     }
-    if (getComputerChoice() === 'rock') {
-        playerScore += 1;
-    }
-    if (playerScore === winningScore) {
-        rock.style.opacity = "0.5";
-        paper.style.opacity = "0.5";
-        scissors.style.opacity = "0.5";
-
-        rock.disabled = "true";
-        paper.disabled = "true";
-        scissors.disabled = "true";
-        rock.style.cursor = "not-allowed";
-        paper.style.cursor = "not-allowed";
-        scissors.style.cursor = "not-allowed";
-        playerDisplay.style.color = "green";
-        computerDisplay.style.color = "red";
-    }
-    if (computerScore === winningScore) {
-        rock.style.opacity = "0.5";
-        paper.style.opacity = "0.5";
-        scissors.style.opacity = "0.5";
-
-        rock.disabled = "true";
-        paper.disabled = "true";
-        scissors.disabled = "true";
-        rock.style.cursor = "not-allowed";
-        paper.style.cursor = "not-allowed";
-        scissors.style.cursor = "not-allowed";
-        playerDisplay.style.color = "red";
-        computerDisplay.style.color = "green";
-    }
-
-    playerDisplay.innerText = playerScore;
-    computerDisplay.innerText = computerScore;
+}
 
 
-})
-
-
-scissors.addEventListener('click', function () {
-
-    getComputerChoice()
-
-    if (getComputerChoice() === 'rock') {
-        computerScore += 1;
-    }
-    if (getComputerChoice() === 'paper') {
-        playerScore += 1;
-    }
-    if (playerScore === winningScore) {
-        rock.style.opacity = "0.5";
-        paper.style.opacity = "0.5";
-        scissors.style.opacity = "0.5";
-
-        rock.disabled = "true";
-        paper.disabled = "true";
-        scissors.disabled = "true";
-        rock.style.cursor = "not-allowed";
-        paper.style.cursor = "not-allowed";
-        scissors.style.cursor = "not-allowed";
-        playerDisplay.style.color = "green";
-        computerDisplay.style.color = "red";
-    }
-    if (computerScore === winningScore) {
-        rock.style.opacity = "0.5";
-        paper.style.opacity = "0.5";
-        scissors.style.opacity = "0.5";
-
-        rock.disabled = "true";
-        paper.disabled = "true";
-        scissors.disabled = "true";
-        rock.style.cursor = "not-allowed";
-        paper.style.cursor = "not-allowed";
-        scissors.style.cursor = "not-allowed";
-        playerDisplay.style.color = "red";
-        computerDisplay.style.color = "green";
+function updateChoices(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case 'ROCK':
+            playerSign.innerHTML = `${codes.rock}`
+            break
+        case 'PAPER':
+            playerSign.innerHTML = `${codes.paper}`
+            break
+        case 'SCISSORS':
+            playerSign.innerHTML = `${codes.scissors}`
+            break
     }
 
-    playerDisplay.innerText = playerScore;
-    computerDisplay.innerText = computerScore;
+    switch (computerSelection) {
+        case 'ROCK':
+            computerSign.innerHTML = `${codes.rock}`
+            break
+        case 'PAPER':
+            computerSign.innerHTML = `${codes.paper}`
+            break
+        case 'SCISSORS':
+            computerSign.innerHTML = `${codes.scissors}`
+            break
+    }
+}
+
+function updateScore() {
+    if (roundWinner === 'tie') {
+        scoreInfo.textContent = "It's a tie!"
+    } else if (roundWinner === 'player') {
+        scoreInfo.textContent = 'You won!'
+    } else if (roundWinner === 'computer') {
+        scoreInfo.textContent = 'You lost!'
+    }
+
+    playerScoreDisplay.textContent = `Player: ${playerScore}`
+    computerScoreDisplay.textContent = `Computer: ${computerScore}`
+}
 
 
-})
+
+
+function updateScoreMessage(winner, playerSelection, computerSelection) {
+    if (winner === 'player') {
+        scoreMessage.textContent = `${playerSelection} beats ${computerSelection.toLowerCase()}`
+        return
+    }
+    if (winner === 'computer') {
+        scoreMessage.textContent = `${playerSelection} is beaten by ${computerSelection.toLowerCase()}`
+        return
+    }
+
+    scoreMessage.textContent = `${playerSelection} ties with ${computerSelection.toLowerCase()}`
+}
+
+
+
+
+
+
+
